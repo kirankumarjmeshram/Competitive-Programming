a1ce5431bef9bbbb3ad136971cb3fbd4d0e86abe
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-11-08 20:38:37 +0530

    get first set bit

---
+++ GFG DSA/Bit Magic/GetSetBit.java
@@ -0,0 +1,22 @@
+/**
+ * GetSetBit
+ * https://practice.geeksforgeeks.org/batch/CP-36/track/CP-33BitMagic/problem/find-first-set-bit-1587115620
+ * get position of last '1' in binary form of num from last
+ * eg 12 => 1100 => here last occur of 1 position is 3
+ */
+public class GetSetBit {
+
+    public static int getFirstSetBit(int n){
+            
+        // Your code here
+        int count = 1;
+        if(n==0) return 0;
+        while (n > 0){
+            if((n & 1) > 0) return count;
+            count ++;
+            n = n >>1;
+        }
+        return -1;
+            
+    }
+}
