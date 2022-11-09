1fa5a9ad558b5d958d39b04c0536cb17134cf6dc
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-11-09 21:15:56 +0530

    Check whether K-th bit is set or not

---
+++ GFG DSA/Bit Magic/CheckBit.java
@@ -0,0 +1,27 @@
+/*
+ * Given a number N and a bit number K, check if Kth 
+ * bit of N is set or not. A bit is called set if it is 1.
+ *  Position of set bit '1' should be indexed starting 
+ * with 0 from LSB side in binary representation of the number.
+ * 
+*/
+public class CheckBit {
+    static boolean checkKthBit(int n, int k)
+    {
+       // with left shift operator
+       int x = (1<<k) & n;
+       // Your code here
+       if(x > 0){
+           return true;
+       }else{
+           return false;
+       }
+    //    with right shift
+    // int X = (n>>K) & 1;
+    // if(X == 1) {
+    //     return true;
+    // }else {
+    //     return false;
+    // }
+    }
+}
