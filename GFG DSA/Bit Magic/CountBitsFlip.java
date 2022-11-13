9c3a0063a03bbeb7d7ea4cfccc3ddd8a2d13f7d5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-11-13 23:52:18 +0530

    Bit Difference

--- GFG DSA/Bit Magic/CountBitsFlip.java
+++ GFG DSA/Bit Magic/CountBitsFlip.java
@@ -1,7 +1,20 @@
 /**
  * CountBitsFlip
+ * You are given two numbers A and B. The task is to count the number of bits 
+ * needed to be flipped to convert A to B.
  */
 public class CountBitsFlip {
-
+    public static int countBitsFlip(int a, int b){
+        return setBit(a^b); 
+    }
+    
+    public static int setBit(int c){
+        int count = 0;
+        while(c != 0){
+            count++;
+            c = c & (c-1);
+        }
+        return count;
+    }
     
 }
